812a6d3634c3cf53c2f021da106216b35d4b9634
Shannon Nolan <shannonnolan17@gmail.com>
2018-04-04 18:21:13 -0700

    Added solution

---
+++ get_products_of_all_ints_except_at_index.rb
@@ -0,0 +1,16 @@
+def my_function(arg)
+  final = 1
+  results = []
+  arg.each_with_index do |num, index|
+    arg.delete(num)
+      arg.each do |new_num|
+        final *= new_num
+      end
+    results << final
+    final = 1
+    arg.insert(index, num)
+  end
+
+  results
+
+end
